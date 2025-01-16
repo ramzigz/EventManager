071ae2a01b7a126dd487cedc7c39efc3c1de1caf
ramzigz <ramzi.guizani.tn@gmail.com>
2025-01-16 12:22:03 +0100

    chore: back-end config

---
+++ back-end/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  coverageDirectory: 'coverage',
+  collectCoverage: true
+};
