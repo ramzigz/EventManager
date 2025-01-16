0395a174d988f88b82d35367187413bcb8750b4c
ramzigz <ramzi.guizani.tn@gmail.com>
2025-01-16 12:33:15 +0100

    feat: add routes validation

---
+++ back-end/src/validations/events.validations.ts
@@ -0,0 +1,16 @@
+import { body, param } from 'express-validator';
+
+export const validateCreateEvent = [
+  body('title').isString().notEmpty().withMessage('Title is required'),
+  body('date').isISO8601().withMessage('Date must be a valid ISO 8601 string'),
+];
+
+export const validateEventId = [
+  param('id').isUUID().withMessage('ID must be a valid UUID'),
+];
+
+export const validateUpdateEvent = [
+  param('id').isUUID().withMessage('ID must be a valid UUID'),
+  body('title').optional().isString(),
+  body('date').optional().isISO8601(),
+];
